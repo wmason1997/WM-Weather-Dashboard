63d5efc561cee8f18ae86d6a0b0f28a91efc5ffc
wmason1997 <williamcmason1997@gmail.com>
2023-11-08 19:51:24 -0800

    Added some more format to index.html file and added the relevant website links to reference for direct geocoding.

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 var APIKey = "00cae84c06c0ac5953b157e62ece3d01";
+
+// Consult https://openweathermap.org/forecast5 and https://openweathermap.org/api/geocoding-api for the city search (direct geocoding)
