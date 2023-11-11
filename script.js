4825b069b919471cee7a2f8e32bb9cdc440ec94a
wmason1997 <williamcmason1997@gmail.com>
2023-11-11 10:10:32 -0800

    added a lot of functionality with Mila's help

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ var sanDiegoLon = -117.161087;
 var submitButton = document.getElementById('search-button');
 var inputCity = document.getElementById('user-input-city');
 
+var immediate_weather_placeholder = document.getElementById("immediate-weather");
+var searchedCities = document.getElementById('searched-cities');
+
+var fiveday_forecast_placeholder = document.getElementById("five-day-forecast");
+var cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+cityHistory.forEach(renderButtons);
+
+function renderButtons(city){
+    var button = document.createElement('button');
+    button.value = city;
+    button.textContent = city;
+    button.addEventListener("click", function() { 
+        directGeocode(city);
+    });
+    searchedCities.append(button);
+};
 
 submitButton.addEventListener("click", function (event) {
     event.preventDefault();
@@ -26,34 +42,45 @@ submitButton.addEventListener("click", function (event) {
 
 // Direct geocoding
 async function directGeocode(cityName) {
-    var searchURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey;
-
+    //var searchURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + APIKey;
+    var searchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + APIKey;
+    immediate_weather_placeholder.innerHTML = "";
     try {
         const response = await fetch(searchURL);
         const data = await response.json();
-
-        if (data.length > 0) {
-            console.log(data);
-            console.log(data[0].lat);
-            console.log(data[0].lon);
-
-            return { lat: data[0].lat, lon: data[0].lon };
-        } else {
-            console.error('No data found for the city:', cityName);
-            return null;
+        console.log(data);
+        if (!cityHistory.includes(data.name)) {
+            cityHistory.push(data.name);
+            localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
+            renderButtons(data.name);
         }
+
+        // Append to current weather section
+        const city = document.createElement("h1");
+        city.textContent = data.name;
+        immediate_weather_placeholder.append(city);
+
+        // Append weather icons
+        const icon = document.createElement('img');
+        icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        immediate_weather_placeholder.append(icon);
+
+        // Append 
+
+        getWeather(cityName);
+
     } catch (error) {
             console.error('Error:', error);
             return null;
     }
 }
 
-directGeocode('Seattle'); // gets lat and long of Seattle
+// directGeocode('Seattle'); // gets lat and long of Seattle
 
 
-function getWeather(inputLat, inputLon) {
+function getWeather(cityName) {
     // fetch request gets a 5 day weather forecast in 3-hour increments from OpenWeatherMap API
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='+inputLat+'&lon=' + inputLon + '&appid=' + APIKey;
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + APIKey;
     // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
     fetch(requestUrl)
@@ -63,26 +90,39 @@ function getWeather(inputLat, inputLon) {
         .then(function (data){
             console.log(data);
             console.log(isolateAfternoons(data));
+            const response = isolateAfternoons(data);
+            response.forEach(function(day) { 
+                const city = document.createElement("h2");
+                city.textContent = day.dt_txt.slice(0,10);
+                fiveday_forecast_placeholder.append(city);
+
+                // wind 
+                // humidity etc.
+        
+                const icon = document.createElement('img');
+                icon.src = `http://openweathermap.org/img/w/${day.weather[0].icon}.png`;
+                fiveday_forecast_placeholder.append(icon);
+            })
         });
 }
 
 
 
-getWeather(sanDiegoLat, sanDiegoLon);
+// getWeather(sanDiegoLat, sanDiegoLon);
 
-var temporaryCoordinates = directGeocode('Boston');
+//var temporaryCoordinates = directGeocode('Boston');
 
-console.log(temporaryCoordinates);
+//console.log(temporaryCoordinates);
 
 //getWeather(directGeocode("Chicago"));
 
 // Usage example
-directGeocode("Chicago")
-    .then(function (coordinates) {
-        if (coordinates) {
-            getWeather(coordinates.lat, coordinates.lon);
-        }
-    });
+// directGeocode("Chicago")
+//     .then(function (coordinates) {
+//         if (coordinates) {
+//             getWeather(coordinates.lat, coordinates.lon);
+//         }
+//     });
 
 
 // getWeather(temporaryCoordinates);
